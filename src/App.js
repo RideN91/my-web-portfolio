2c6184d5e34a3cf46544e731d7d888965d0276ab
DK <kovalovsky911@gmail.com>
2023-12-08 01:12:21 +0100

    First commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                {/* <img src={logo} className="App-logo" alt="logo" /> */}
+                <h1>AHOJ</h1>
+            </header>
+        </div>
+    );
+}
+
+export default App;
