db6d6c4a4c4d5fd86fd5d89ffeb45aa4d14d8949
DK <kovalovsky911@gmail.com>
2024-01-01 18:59:41 +0100

    Responsive hambureger menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,60 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import { GiHamburgerMenu, GiElectric } from "react-icons/gi";
 
 const Navbar = () => {
+    const [isMenuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!isMenuOpen);
+    }
+
     return (
         <>
-        <nav>
-            <ul className='sticky z-[20] mx-auto flex w-full items-end justify-end px-6 py-16 space-x-8 md:justify-end sm:justify-center'>
-                <h1 className="text-xl flex-grow">DK</h1>
-                <li>
-                    <a className="text-xl" href="/">Home</a>
-                </li>
-                <li>
-                    <a className="text-xl" href="/about-me">About</a>
-                </li>
-                <li>
-                    <a className="text-xl" href="/portfolio">Portfolio</a>
-                </li>
-                <li>
-                    <a className="text-xl" href="/contact">Contact</a>
-                </li>
-            </ul>
-        </nav>
+            <nav className='mx-auto flex w-full max-w-[1240px] justify-end px-6 py-16 space-x-8 md:justify-end'>
+                <div className="flex-grow">
+                    <GiElectric size={32} />
+                </div>
+
+                <div className="hidden md:block"> {/* Ensure this is hidden on small screens */}
+                    <ul className="flex">
+                        <li>
+                            <a className="text-md text-gray-600 hover:text-gray-800" href="/">Home</a>
+                        </li>
+                        <li>
+                            <a className="text-md text-gray-600 hover:text-gray-800" href="/about-me">About</a>
+                        </li>
+                        <li>
+                            <a className="text-md text-gray-600 hover:text-gray-800" href="/portfolio">Portfolio</a>
+                        </li>
+                        <li>
+                            <a className="text-md text-gray-600 hover:text-gray-800" href="/contact">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+
+                <div class="block md:hidden">
+                    <button onClick={toggleMenu}>
+                        <GiHamburgerMenu size={24} />
+                    </button>
+                </div>
+
+                <div className={`md:hidden ${isMenuOpen ? 'block' : 'hidden'}`}>
+                    <ul className="flex-bottom">
+                        <li>
+                            <a className="text-md" href="/">Home</a>
+                        </li>
+                        <li>
+                            <a className="text-md" href="/about-me">About</a>
+                        </li>
+                        <li>
+                            <a className="text-md" href="/portfolio">Portfolio</a>
+                        </li>
+                        <li>
+                            <a className="text-md" href="/contact">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
         </>
     )
 }
