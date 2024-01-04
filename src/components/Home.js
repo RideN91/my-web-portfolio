9fdd78d2d51e3c6773731dff27571bea3330a34f
DK <kovalovsky911@gmail.com>
2024-01-04 22:03:15 +0100

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,25 @@
-export default function Home() {
+import React, {useState, useEffect } from 'react';
+
+const Home = () => {
+    const textOptions = ['Daniel', 'Front End Developer'];
+    const [dynamicTextIndex, setDynamicTextIndex] = useState(0);
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setDynamicTextIndex((prevIndex) => 
+            prevIndex === textOptions.length - 1 ? 0 : prevIndex + 1
+            );
+        }, 3000);
+
+        return () => clearInterval(intervalId);
+    }, []);
+
     return (
-        <div className="px-12 max-w-[1240px]">
-            <h1>Home</h1>
+        <div className="container mx-auto px-12 w-full">
+            <h1 className="text-4xl font-bold mb-4">Hi, I'm {textOptions[dynamicTextIndex]}.</h1>
         </div>
     );
+
 }
+
+export default Home;
