dd895ddb3f07b5df869d3be9b42e755fe93643c4
DK <kovalovsky911@gmail.com>
2024-01-05 23:55:09 +0100

    Update
    I have deleted script for "typing" and i have used react-type-animation instead. Update of the pic ( lighter), finally sorted the position of the text and the picture in the middle.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import TypingText from './TypingText';
+import { TypeAnimation } from 'react-type-animation';
 
 const Home = () => {
     const textOptions = ['<Daniel>!', '<Front End Developer>!'];
@@ -16,21 +16,34 @@ const Home = () => {
     }, []);
 
     return (
-        <div className="container mx-auto mt-60 relative flex items-center justify-center">
-                <div className="fixed top-48 left-64 mr-8">
-                    <img 
-                        src="./Autoportret.png"
+        <>
+            <div className="max-w-[800px] w-full h-screen flex">
+                <div className="static absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+                    <img
+                        src="./Face.png"
                         alt="Me"
-                        className="overflow-hidden rounded-md w-128 h-96 fixed"
+                        className="opacity-75"
                         style={{ clipPath: 'circle(50% at 50% 50%)' }}
                     />
                 </div>
-                <div className="ml-0">
-                    <h1 className="text-4xl font-bold mb-4 fixed">
-                        Hello, I'm <TypingText text={textOptions[dynamicTextIndex]} />
-                    </h1>
+
+                <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 font-bold"
+                     id="typing-text"
+                    >
+                    <TypeAnimation
+                        sequence={[
+                            'Hello, My name is Daniel!',
+                            1000,
+                            'I am a front-end developer.',
+                            1000,
+                        ]}
+                        speed={50}
+                        style={{ fontSize: '2em' }}
+                        repeat={Infinity}
+                    />
                 </div>
-        </div>
+            </div>
+        </>
     );
 }
 
