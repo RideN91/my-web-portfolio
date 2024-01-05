9f100744c02aca9a561116c98a7e42c106c99cfc
DK <kovalovsky911@gmail.com>
2024-01-05 02:14:55 +0100

    Positioning
    Still not sorted, but autoportret fixed, will change the pic anyway..

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 import TypingText from './TypingText';
 
 const Home = () => {
-    const textOptions = ['Daniel!', 'Front End Developer!'];
+    const textOptions = ['<Daniel>!', '<Front End Developer>!'];
     const [dynamicTextIndex, setDynamicTextIndex] = React.useState(0);
 
     React.useEffect(() => {
@@ -16,11 +16,22 @@ const Home = () => {
     }, []);
 
     return (
-        <div className="container mx-auto px-12 w-full">
-            <h1 className="text-4xl font-bold mb-4">Hi, I'm <TypingText text={textOptions[dynamicTextIndex]} /></h1>
+        <div className="container mx-auto mt-60 relative flex items-center justify-center">
+                <div className="fixed top-48 left-64 mr-8">
+                    <img 
+                        src="./Autoportret.png"
+                        alt="Me"
+                        className="overflow-hidden rounded-md w-128 h-96 fixed"
+                        style={{ clipPath: 'circle(50% at 50% 50%)' }}
+                    />
+                </div>
+                <div className="ml-0">
+                    <h1 className="text-4xl font-bold mb-4 fixed">
+                        Hello, I'm <TypingText text={textOptions[dynamicTextIndex]} />
+                    </h1>
+                </div>
         </div>
     );
-
 }
 
 export default Home;
